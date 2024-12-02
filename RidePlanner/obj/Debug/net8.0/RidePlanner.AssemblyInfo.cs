c7aaffa5f697b7bdb7e0194e334ef3c87e2bf6da
oltahasimja <164362593+oltahasimjaa@users.noreply.github.com>
2024-12-02 01:31:11 +0100

    DatabaseConnection
    RidePlannerDb

--- RidePlanner/obj/Debug/net8.0/RidePlanner.AssemblyInfo.cs
+++ RidePlanner/obj/Debug/net8.0/RidePlanner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RidePlanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dbd7c4b8c49190333d01544ab064dbb86a45d28")]
 [assembly: System.Reflection.AssemblyProductAttribute("RidePlanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RidePlanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
