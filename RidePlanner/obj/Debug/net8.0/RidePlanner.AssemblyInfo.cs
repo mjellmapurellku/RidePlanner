05fe2e19e125e5e6f9e98b076ca0136f2ec41914
mjellmapurellku <mjellmapurellku@gmail.com>
2025-01-01 17:39:48 +0100

    Taxi Booking view
    View for Taxi Booking

--- RidePlanner/obj/Debug/net8.0/RidePlanner.AssemblyInfo.cs
+++ RidePlanner/obj/Debug/net8.0/RidePlanner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RidePlanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6f2921be2a0a91603e31dd1b5d6cb1bcf3ff95a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+985177ac3e9b7e30300bbc617d6b43a9dec79a59")]
 [assembly: System.Reflection.AssemblyProductAttribute("RidePlanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RidePlanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
