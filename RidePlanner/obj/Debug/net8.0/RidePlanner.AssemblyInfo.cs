a97bade5b2e09fd89b456de5e84dbdc0a4f99b71
GretaGashi <gg68557@ubt-uni.net>
2025-01-03 02:20:25 +0100

    deleted extra files

--- RidePlanner/obj/Debug/net8.0/RidePlanner.AssemblyInfo.cs
+++ RidePlanner/obj/Debug/net8.0/RidePlanner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RidePlanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4768d43e50a2353e8f4cf908e6f0a9dcf8a88b02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9eddd7f617570e4f171db065f6f8e3d9c4f5d0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("RidePlanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RidePlanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- RidePlanner/obj/Release/net8.0/RidePlanner.AssemblyInfo.cs
+++ RidePlanner/obj/Release/net8.0/RidePlanner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RidePlanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d128fc870105790ece84a86ac0b6b4486703449a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9eddd7f617570e4f171db065f6f8e3d9c4f5d0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("RidePlanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RidePlanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
