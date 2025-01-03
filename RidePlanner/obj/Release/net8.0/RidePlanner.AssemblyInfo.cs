937f41a508c78b44bc803947ee10dd227b2f5e2a
GretaGashi <gg68557@ubt-uni.net>
2025-01-03 02:55:18 +0100

    Re-designed Taxi Booking & Taxi Reservation

--- RidePlanner/obj/Release/net8.0/RidePlanner.AssemblyInfo.cs
+++ RidePlanner/obj/Release/net8.0/RidePlanner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RidePlanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b589d7e3e59f1e9fb263bd2f2099a051c8ffbe11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e32fb526bfa6941841a124d12a444663469bbd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("RidePlanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RidePlanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
